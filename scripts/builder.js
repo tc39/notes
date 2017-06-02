0a7298af87aaa5bb9274c4a26c5b127ba9b50162
Rick Waldron <waldron.rick@gmail.com>
2017-06-02 12:13:39 -0400

    simplify builder
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- scripts/builder.js
+++ scripts/builder.js
@@ -2,8 +2,6 @@
 
 // System objects
 const fs = require("fs");
-const cp = require("child_process");
-const path = require("path");
 
 // Third party objects
 const ct = require("common-tags");
@@ -16,43 +14,21 @@ const remarkable = new Remarkable({
   linkify: true,
 });
 
-
 glob("./es*/**/*.md", (error, results) => {
-  const aliases = {};
-  const titles = {};
   const links = [];
-  const contents = results.reverse().reduce((accum, file) => {
-    const source = fs.readFileSync(file, "utf8");
-    const name = file.replace(/\.\/es.\//, "");
-    aliases[name] = name.replace(/(\d{4}-\d{2})\//, "$1_").replace(".md", ".html");
-    titles[name] = source.split('\n')[0].replace("# ", "").trim();
-    accum[name] = source;
-    return accum;
-  }, {});
 
+  results.reverse().forEach(file => {
+    const content = fs.readFileSync(file, "utf8");
+    const alias = file.replace(/\.\/es.\//, "");
+    const fileName = alias.replace(/(\d{4}-\d{2})\//, "$1_").replace(".md", ".html");
+    const title = content.split('\n')[0].replace("# ", "").trim();
 
-  // console.log(index(Object.keys(contents)));
-// console.log(titles);
-  // console.log(aliases);
-
-  // console.log(contents["2017-05/may-25.md"]);
-
-  Object.keys(aliases).forEach(alias => {
-    const fileName = aliases[alias];
-    const title = titles[alias];
-    const content = contents[alias];
     fs.writeFileSync(fileName, makePage({ title, content }));
-
-    // links.push(remarkable.render(`[${title.replace(" Meeting Notes", "")}](${fileName})`));
     links.push(`- [${title.replace(" Meeting Notes", "")}](${fileName})`);
   });
 
-
-  const index = makeIndex({ links });
-
-  fs.writeFileSync("index.html", index);
+  fs.writeFileSync("index.html", makeIndex({ links }));
 });
-
 
 function makeIndex({links}) {
   return ct.stripIndent`
@@ -72,5 +48,3 @@ function makePage({title, content}) {
 ${remarkable.render(content)}
 `;
 }
-
-

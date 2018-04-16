598fa9cd89ff758ad24eaf05ae27bc4d0c0909c0
Leo Balter <leonardo.balter@gmail.com>
2018-04-16 14:31:38 -0400

    Prepend the filename to each summary link

--- scripts/summary.js
+++ scripts/summary.js
@@ -7,6 +7,7 @@ const path = require("path");
 // Third party objects
 const glob = require("glob");
 const toc = require("markdown-toc");
+const mdlink = require("markdown-link", "mdlink");
 const argv = require("minimist")(process.argv.slice(2), {
     string: ["_"]
 });
@@ -30,18 +31,30 @@ function main() {
 
         const writable = fs.createWriteStream(`${folder}/summary.md`, { flags: 'w' });
         const filePattern = /^\w*-\d+\.[mM][dD]$/;
-        const notes = results.filter(file => {
-            return filePattern.test(path.basename(file));
-        });
 
-        notes.forEach(file => {
+        results.forEach(file => {
+            if (!filePattern.test(path.basename(file))) {
+                return;
+            }
             const contents = fs.readFileSync(file, "utf8");
-            const rendered = toc(contents, {
+            const fileToc = toc(contents, {
                 filter(str, ele) {
                     return ele.lvl < 3;
                 },
-                append: "\n"
-            }).content;
+                append: "\n",
+                linkify({ lvl }, title, slug) {
+                    // prepends the filename to the link
+                    let content = mdlink(title, `${path.basename(file)}#${slug}`);
+                    return {
+                        content,
+
+                        // lvl is required to find the highest level links and order it
+                        lvl
+                    };;
+                }
+            });
+
+            const rendered = fileToc.content;
 
             writable.write(rendered);
         });
